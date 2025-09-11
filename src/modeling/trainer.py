40c0cf9d7ead6994d700d57b13cb5592d843da57
jayyoung <hejingyang@stu.pku.edu.cn>
2025-09-11 20:30:47 +0800

    catboost & lightgbm & adaboost

--- src/modeling/trainer.py
+++ src/modeling/trainer.py
@@ -1,4 +1,8 @@
 import xgboost as xgb
+import catboost as cb
+import lightgbm as lgb
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.tree import DecisionTreeRegressor
 import joblib
 import os
 import logging
@@ -14,6 +18,19 @@ class ModelTrainer:
         """根据配置初始化模型，未来可扩展支持不同类型的模型。"""
         if self.model_config.type == 'xgboost':
             return xgb.XGBRegressor(**self.model_config.params)
+        elif self.model_config.type == 'catboost':
+            return cb.CatBoostRegressor(**self.model_config.params)
+        elif self.model_config.type == 'lightgbm':
+            return lgb.LGBMRegressor(**self.model_config.params)
+        elif self.model_config.type == 'adaboost':
+            estimator = DecisionTreeRegressor(max_depth=self.model_config.params.get('max_depth', 8))
+            return AdaBoostRegressor(
+                estimator=estimator,
+                n_estimators=self.model_config.params.get('n_estimators', 1000),
+                learning_rate=self.model_config.params.get('learning_rate', 0.05),
+                loss=self.model_config.params.get('loss', 'linear'),
+                random_state=self.model_config.params.get('random_state', 42)
+            )
         else:
             raise ValueError(f"不支持的模型类型: {self.model_config.type}")
 
@@ -33,11 +50,24 @@ class ModelTrainer:
             )
             model = self._get_model()
             # 训练时使用早停机制
-            model.fit(
-                X_tr, y_tr,
-                eval_set=[(X_val, y_val)],
-                verbose=False
-            )
+            if self.model_config.type == 'lightgbm':
+                model.fit(
+                    X_tr, y_tr, eval_set=[(X_val, y_val)],
+                    callbacks=[
+                        lgb.log_evaluation(-1)       # -1 → no logging, or use 100 for every 100 rounds
+                    ]
+                
+                )
+            elif self.model_config.type == 'adaboost':
+                model.fit(
+                    X_tr, y_tr,
+                )
+            else:
+                model.fit(
+                    X_tr, y_tr, eval_set=[(X_val, y_val)], 
+                    verbose=False
+                )
+
             model_path = os.path.join(self.model_dir, f'model_target_{i}.joblib')
             joblib.dump(model, model_path)
             logging.info(f"模型已保存至: {model_path}")
